f20f8e20e255bb0c98c851f85ee7a7de19ac25a4
kamatealif <kamatealip1@gmail.com>
2025-01-22 20:38:43 +0530

    Add Flask blog structure with home and about pages, and styling

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+
+# dummy data 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 22, 2025'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 23, 2025'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    name = "alif"
-    return f"<h1>Hello {name}!</h1>"
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    page = "about"
-    return "<h1>About {page}</h1>"
+    return render_template("about.html", title="About")
 
 if __name__ =="__main__":
     app.run(debug=True)
